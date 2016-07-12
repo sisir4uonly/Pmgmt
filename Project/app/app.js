62f684b2a79ffb87f096d13ddfce51b4f0fee9a7
Sisir <Sisir4uonly@gmail.com>
2016-07-12 14:17:23 -0400

    Initial Detail View Added

--- Project/app/app.js
+++ Project/app/app.js
@@ -3,5 +3,35 @@
 (function () {
     "use strict";
     var app = angular.module("productManagement",
-                             ["common.services", "productResourceMock"]);
+                             ["common.services",
+                              "ui.router",
+                              "productResourceMock"]);
+    app.config(["$stateProvider",
+                "$urlRouterProvider",
+       function ($stateProvider, $urlRouterProvider) {
+
+           $urlRouterProvider.otherwise("/");
+
+           $stateProvider
+               .state("home", {
+                    url: "/",
+                    templateUrl : "app/welcomeView.html"
+                })
+               .state("productList", {
+                    url: "/products",
+                    templateUrl: "app/products/productListView.html",            
+                    controller :"ProductListCtrl as PListVM"
+                })
+               .state("productEdit", {
+                   url: "/products/edit/:productId",
+                   templateUrl: "app/products/productEditView.html",
+                   controller: "ProductEditCtrl as PListVM"
+               })
+               .state("productDetail", {
+                url: "/products/:productId",
+                templateUrl: "app/products/productDetailView.html",
+                controller: "ProductDetailCtrl as PListVM"
+                   //productDetailCtrl  controller: "ProductDetailCtrl as PListVM"
+            })
+    }]);
 }());
