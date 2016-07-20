9eb54a9164469ca3ea2daebdb50bfe64eb9e229a
Sisir <Sisir4uonly@gmail.com>
2016-07-20 11:31:21 -0400

    Initial Edit page added

--- Project/app/app.js
+++ Project/app/app.js
@@ -25,7 +25,26 @@
                .state("productEdit", {
                    url: "/products/edit/:productId",
                    templateUrl: "app/products/productEditView.html",
-                   controller: "ProductEditCtrl as PListVM"
+                   controller: "ProductEditCtrl as PListVM",
+                   resolve: {
+                       productResourceVar: "productResource",
+                       product: function (productResourceVar, $stateParams) {
+                           var productId = $stateParams.productId;
+                           return productResourceVar.get({ productId: productId }).$promise;
+                       }
+                   }
+               })
+               .state("productEdit.info", {
+                   url: "/info",
+                   templateUrl : "app/products/productEditInfoView.html",
+               })
+               .state("productEdit.price", {
+                   url: "/info",
+                   templateUrl: "app/products/productEditPriceView.html",
+               })
+               .state("productEdit.tags", {
+                   url: "/info",
+                   templateUrl: "app/products/productEditTagsView.html",
                })
                .state("productDetail", {
                 url: "/products/:productId",
---
+++ Project/app/products/productEditCtrl.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="../../js/angular.min.js" />
+
+(function myfunction() {
+    "use strict";
+
+    angular.module("productManagement")
+            .controller("ProductEditCtrl",
+                        ["product", ProductEditCtrl]);
+
+    function ProductEditCtrl(product) {
+
+        var PListVM = this;
+        PListVM.product = product;
+
+        if (PListVM.product && PListVM.product.productId) {
+            PListVM.title = "Edit : " + PListVM.product.productName;
+        }
+        else {
+            PListVM.title = "New Product";
+        }
+    }
+})();
